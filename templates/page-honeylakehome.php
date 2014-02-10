bfb4375781666a320710964472e52cff62e0bc4c
fenhoff <afenhoff@gmail.com>
2014-02-09 23:58:20 -0500

    pulled home page vimeo id into magic field

--- templates/page-honeylakehome.php
+++ templates/page-honeylakehome.php
@@ -21,14 +21,20 @@ get_header();
 include 'slider.php';
 
 	wp_nav_menu( array('menu' => basename(get_permalink()), 'container' => 'div', 
-				 'container_class' => 'overlay-menu') ); ?>
+				 'container_class' => 'overlay-menu') ); 
 
+	$vimeoID = get('vimeo_url'); 
+	$tags = array("<p>", "</p>");
+    $vimeoID = str_replace($tags, "", $vimeoID);
+    ?>
+    
 	<div class="videoOverlay">
 		<a href="#" class="video-close-btn" onclick="return false;">Close</a>
 		<div class="vid-container">
-	        <iframe src="http://player.vimeo.com/video/81975933?title=0&byline=0&portrait=0&color=ffffff"
+	        <?php echo '
+	        <iframe src="http://player.vimeo.com/video/' . $vimeoID . '?title=0&byline=0&portrait=0&color=ffffff"
 	           width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
-	       </iframe>
+	       </iframe>'; ?>
 	   </div>
 	</div>
 	</div>
