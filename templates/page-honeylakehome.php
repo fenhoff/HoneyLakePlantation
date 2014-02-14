23cf0883ca40bca9f96e4a2188a3598e0dfef8b9
mdefazio <hellodefazio@gmail.com>
2014-02-14 12:04:26 -0500

    Footer, Popup, Video
    Updated twitter icon hover,
    Video centered
    Email popup fixed (hopefully)

--- templates/page-honeylakehome.php
+++ templates/page-honeylakehome.php
@@ -15,50 +15,50 @@ if ( !defined('ABSPATH')) exit;
  * @copyright      2012-2013 Styledthemes.com
  * @license        license.txt
  */
- 
-get_header(); 
 
-include 'slider.php';
-	wp_nav_menu( array('menu' => basename(get_permalink()), 'container' => 'div', 
-				 'container_class' => 'overlay-menu') ); 
+   get_header(); 
 
-	$vimeoID = get('vimeo_url'); 
-	$tags = array("<p>", "</p>");
-    $vimeoID = str_replace($tags, "", $vimeoID);
-    ?>
+   include 'slider.php';
+   wp_nav_menu( array('menu' => basename(get_permalink()), 'container' => 'div', 
+   	'container_class' => 'overlay-menu') ); 
 
-	<div class="videoOverlay">
-			
-<?php if (isset($_COOKIE['newvisitor'])) { ?>
+   $vimeoID = get('vimeo_url'); 
+   $tags = array("<p>", "</p>");
+   $vimeoID = str_replace($tags, "", $vimeoID);
+   ?>
+
+   <?php if (isset($_COOKIE['newvisitor'])) { ?>
+
+   <script src="https://static-interlogyllc.netdna-ssl.com/static/feedback2.js?3.2.570" type="text/javascript">
+
+   new JotformFeedback({
+   	formId:'40440259074147',
+   	base:'https://secure.jotform.us/',
+   	windowTitle:'Email PopUp',
+
+   	background:'#FFA500',
+   	fontColor:'#FFFFFF',
+   	type:false,
+   	height:500,
+   	width:700,
+
+   	openOnLoad:true
+   });
+   
+   </script>
+
+   <?php } ?>
 
 
-<script src="https://static-interlogyllc.netdna-ssl.com/static/feedback2.js?3.2.570" type="text/javascript">
+   <div class="videoOverlay">
 
-new JotformFeedback({
-formId:'40440259074147',
-base:'https://secure.jotform.us/',
-windowTitle:'Email PopUp',
-
-background:'#FFA500',
-fontColor:'#FFFFFF',
-type:false,
-height:500,
-width:700,
-
-openOnLoad:true
-});
-</script>
-<?php } ?>
-
-
-		<a href="#" class="video-close-btn" onclick="return false;">Close</a>
-		<div class="vid-container">
-	        <?php echo '
-	        <iframe src="http://player.vimeo.com/video/' . $vimeoID . '?title=0&byline=0&portrait=0&color=ffffff"
-	           width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
-	       </iframe>'; ?>
-	   </div>
-	</div>
-	</div>
+   	<a href="#" class="video-close-btn" onclick="return false;">Close</a>
+   	<div class="vid-container">
+   		<?php echo '
+   		<iframe src="http://player.vimeo.com/video/' . $vimeoID . '?title=0&byline=0&portrait=0&color=ffffff"
+   		width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+   	</iframe>'; ?>
+   </div>
+</div>
+</div>
 <?php get_footer(); ?>
-	
